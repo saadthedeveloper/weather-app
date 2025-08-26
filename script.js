1f200350749eb112a981d7df83615f0391e994f3
saadthedeveloper <m.saad23145@gmail.com>
2025-08-26 09:03:22 -0700

    Worked on Logic

--- script.js
+++ script.js
@@ -1,2 +1,53 @@
-let base_Url = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}"
-let APIKey = "35dd47446cb0e4d5eb601989c945ed63"
+let base_Url = "https://api.openweathermap.org/data/2.5/weather?q=";
+let APIKeyWeather = "";
+
+let userInputCityName = document.querySelector("#search-div input");
+let searchButton = document.querySelector("#search-div button");
+let weatherIcon = document.querySelector("#weather-icon");
+let tempratureElement = document.querySelector("#Temprature");
+
+searchButton.addEventListener("click", async () => {
+    let URL = `${base_Url}${userInputCityName.value}&appid=${APIKeyWeather}`;
+
+    let dataJSON = await fetch(URL);
+
+    let data = await dataJSON.json();
+
+    if (data.cod === 200) {
+        let temprature = Math.floor((data.main.temp) - 273.5); //Temprature In Celsius
+        let country = data.sys.country;
+        let windSpeed = Math.ceil((data.wind.speed) * 3.6); //Wind Speed In Km/h
+        let weatherDescription = data.weather[0].description;
+
+        tempratureElement.innerText = `${temprature}°C`;
+
+        console.log(temprature, windSpeed, weatherDescription, country);
+        console.log(data);
+        if(weatherDescription.includes("cloud") )
+        {
+            weatherIcon.setAttribute('src', 'Images/clouds.png');
+        }
+        else if(weatherDescription.includes("clear") )
+        {
+            weatherIcon.setAttribute('src', 'Images/sun.png');
+            
+        }
+    }
+    else {
+        console.log("city not found")
+    }
+
+
+
+});
+
+//overcast cloud
+//clear sky
+//scattered clouds
+//few clouds
+//broken clouds
+//moderate rain
+
+
+
+
