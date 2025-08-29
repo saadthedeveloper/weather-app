54c3763a1aaec90f106a76eeb8e4491256753f8a
saadthedeveloper <m.saad23145@gmail.com>
2025-08-29 04:18:34 -0700

    Added Style, Fixed Font Size Bug For Mobile Devices

--- script.js
+++ script.js
@@ -31,16 +31,14 @@ searchButton.addEventListener("click", async () => {
 
 });
 
+
 const displayWeatherData = (data) => {
     if (data.cod === 200) {
-        let temprature = Math.floor((data.main.temp) - 273.5); //Temprature In Celsius
+        let temprature = Math.floor(data.main.temp - 273.15); //Temprature In Celsius
         let country = data.sys.country;
         let windSpeed = Math.ceil((data.wind.speed) * 3.6); //Wind Speed In Km/h
-        let weatherDescription = data.weather[0].description;
+        let weatherDescription = data.weather[0].main.toLowerCase();
         let humidity = data.main.humidity;
-
-
-
 
         tempratureElement.innerText = `${temprature}°C`;
         cityNameDisplayed.innerText = userInputCityName.value.charAt(0).toUpperCase() + userInputCityName.value.slice(1);
@@ -49,29 +47,41 @@ const displayWeatherData = (data) => {
 
         if (cityNameDisplayed.innerText.length === 8 && window.innerWidth > 565) {
             cityNameDisplayed.style.fontSize = "84px";
+            cityNameDisplayed.style.marginTop = "0px";
         }
         else if (cityNameDisplayed.innerText.length >= 9 && window.innerWidth > 565) {
             cityNameDisplayed.style.fontSize = "70px";
+            cityNameDisplayed.style.marginTop = "0px";
         }
         else if (cityNameDisplayed.innerText.length < 8 && window.innerWidth > 565) {
             cityNameDisplayed.style.fontSize = "90px";
+            cityNameDisplayed.style.marginTop = "0px";
+
+        } else if (cityNameDisplayed.innerText.length === 8 && window.innerWidth < 565) {
+            cityNameDisplayed.style.fontSize = "70px";
+            cityNameDisplayed.style.marginTop = "-50px";
+        }
+        else if (cityNameDisplayed.innerText.length >= 9 && window.innerWidth < 565) {
+            cityNameDisplayed.style.fontSize = "50px";
+            cityNameDisplayed.style.marginTop = "-39px";
+
+        }
+        else if (cityNameDisplayed.innerText.length < 8 && window.innerWidth < 565) {
+            cityNameDisplayed.style.fontSize = "90px";
+            cityNameDisplayed.style.marginTop = "-50px";
         }
 
-        console.log(temprature, windSpeed, weatherDescription, country);
-        console.log(data);
+
         if (weatherDescription.includes("cloud")) {
-            weatherIcon.setAttribute('src', 'Images/clouds.png');
-        }
-        else if (weatherDescription.includes("clear")) {
-            weatherIcon.setAttribute('src', 'Images/sun.png');
-
-        }
-        else if (weatherDescription.includes("rain")) {
-            weatherIcon.setAttribute('src', 'Images/drizzle.png');
-
+            weatherIcon.src = 'Images/clouds.png';
+        } else if (weatherDescription.includes("clear")) {
+            weatherIcon.src = 'Images/sun.png';
+        } else if (weatherDescription.includes("rain") || weatherDescription.includes("drizzle") || weatherDescription.includes("drizzle")) {
+            weatherIcon.src = 'Images/drizzle.png';
         } else if (weatherDescription.includes("snow")) {
-            weatherIcon.setAttribute('src', 'Images/snowy.png');
-
+            weatherIcon.src = 'Images/snowy.png';
+        } else {
+            weatherIcon.src = 'Images/mist.png';
         }
     }
     else {
@@ -86,12 +96,6 @@ window.addEventListener("DOMContentLoaded", async () => {
     displayWeatherData(data);
 });
 
-//overcast cloud
-//clear sky
-//scattered clouds
-//few clouds
-//broken clouds
-//moderate rain
 
 
 
